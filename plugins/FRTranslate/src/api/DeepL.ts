57278fb4881e33d1f31c73cd78d95b336f6ccae1
Volcanis <673642+VoIcanis@users.noreply.github.com>
2023-12-04 16:09:56 -0500

    Update DeepL.ts

--- plugins/FRTranslate/src/api/DeepL.ts
+++ plugins/FRTranslate/src/api/DeepL.ts
@@ -2,22 +2,24 @@ import { DeepLResponse } from "../type"
 
 const API_URL = "https://api.deeplx.fun/translate"
 
-const translate = async (text: string, source_lang: string = "auto", target_lang: string, original: boolean = false) => {
+const translate = async (text_data: string, source_lang_data: string = "auto", target_lang_data: string, original: boolean = false) => {
     try {
-        if (original) return { source_lang, text }
+      let dataToSend = {
+        text: text_data,
+        source_lang: source_lang_data,
+        target_lang: target_lang_data
+      }
+      console.log(dataToSend)
+        if (original) return { source_lang_data, text_data }
         const data: DeepLResponse = await (await fetch(API_URL, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({
-                text: text,
-                source_lang: source_lang,
-                target_lang: target_lang
-            })
+            body: JSON.stringify(dataToSend)
         })).json()
         if (data.code !== 200) throw Error(`Failed to translate text from DeepL: ${data.message}`)
-        return { source_lang, text: data.data }
+        return { source_lang_data, text: data.data }
     } catch (e) {
         throw Error(`Failed to fetch from DeepL: ${e}`)
     }
