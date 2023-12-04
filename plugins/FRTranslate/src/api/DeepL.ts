cfc4d69b42aec1e58f4c98bb9338d6fa88461570
Volcanis <673642+VoIcanis@users.noreply.github.com>
2023-12-04 13:34:10 -0500

    init

---
+++ plugins/FRTranslate/src/api/DeepL.ts
@@ -0,0 +1,27 @@
+import { DeepLResponse } from "../type"
+
+const API_URL = "https://api.deeplx.fun/translate"
+
+const translate = async (text: string, source_lang: string = "auto", target_lang: string, original: boolean = false) => {
+    try {
+        if (original) return { source_lang, text }
+        const data: DeepLResponse = await (await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                text: text,
+                source_lang: source_lang,
+                target_lang: target_lang
+            })
+        })).json()
+        if (data.code !== 200) throw Error(`Failed to translate text from DeepL: ${data.message}`)
+        return { source_lang, text: data.data }
+    } catch (e) {
+        throw Error(`Failed to fetch from DeepL: ${e}`)
+    }
+}
+
+export default { translate }
+
