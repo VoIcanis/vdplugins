8425f03df81349fe5c09c9fe33b27091565dc2e8
Volcanis <673642+VoIcanis@users.noreply.github.com>
2023-12-04 15:56:30 -0500

    stuff

--- build.mjs
+++ build.mjs
@@ -1,14 +1,16 @@
-import { readFile, writeFile, readdir } from "fs/promises";
-import { extname } from "path";
-import { createHash } from "crypto";
+import { readFile, writeFile, readdir } from "fs/promises"
+import { createHash } from "crypto"
 
-import { rollup } from "rollup";
-import esbuild from "rollup-plugin-esbuild";
-import commonjs from "@rollup/plugin-commonjs";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import swc from "@swc/core";
+import { rollup } from "rollup"
+import esbuild from "rollup-plugin-esbuild"
+import commonjs from "@rollup/plugin-commonjs"
+import nodeResolve from "@rollup/plugin-node-resolve"
+import swc from "@swc/core"
+import { execSync } from "child_process"
+import { argv } from "process"
 
-const extensions = [".js", ".jsx", ".mjs", ".ts", ".tsx", ".cts", ".mts"];
+let deploy = argv[2]
+if (deploy === "--deploy") deploy = true
 
 /** @type import("rollup").InputPluginOption */
 const plugins = [
@@ -17,21 +19,13 @@ const plugins = [
     {
         name: "swc",
         async transform(code, id) {
-            const ext = extname(id);
-            if (!extensions.includes(ext)) return null;
-
-            const ts = ext.includes("ts");
-            const tsx = ts ? ext.endsWith("x") : undefined;
-            const jsx = !ts ? ext.endsWith("x") : undefined;
-
             const result = await swc.transform(code, {
                 filename: id,
                 jsc: {
                     externalHelpers: true,
                     parser: {
-                        syntax: ts ? "typescript" : "ecmascript",
-                        tsx,
-                        jsx,
+                        syntax: "typescript",
+                        tsx: true,
                     },
                 },
                 env: {
@@ -41,48 +35,62 @@ const plugins = [
                         "transform-arrow-functions",
                     ],
                 },
-            });
-            return result.code;
+            })
+            return result.code
         },
     },
     esbuild({ minify: true }),
-];
+]
 
 for (let plug of await readdir("./plugins")) {
-    const manifest = JSON.parse(await readFile(`./plugins/${plug}/manifest.json`));
-    const outPath = `./dist/${plug}/index.js`;
+    const manifest = JSON.parse(await readFile(`./plugins/${plug}/manifest.json`))
+    const outPath = `./dist/${plug}/index.js`
 
     try {
         const bundle = await rollup({
             input: `./plugins/${plug}/${manifest.main}`,
-            onwarn: () => {},
+            onwarn: () => { },
             plugins,
-        });
-    
+        })
+
         await bundle.write({
             file: outPath,
             globals(id) {
-                if (id.startsWith("@vendetta")) return id.substring(1).replace(/\//g, ".");
+                if (id.startsWith("@vendetta")) return id.substring(1).replace(/\//g, ".")
                 const map = {
                     react: "window.React",
-                };
+                }
 
-                return map[id] || null;
+                return map[id] || null
             },
             format: "iife",
             compact: true,
             exports: "named",
-        });
-        await bundle.close();
-    
-        const toHash = await readFile(outPath);
-        manifest.hash = createHash("sha256").update(toHash).digest("hex");
-        manifest.main = "index.js";
-        await writeFile(`./dist/${plug}/manifest.json`, JSON.stringify(manifest));
-    
-        console.log(`Successfully built ${manifest.name}!`);
+        })
+        await bundle.close()
+
+        const toHash = await readFile(outPath)
+        manifest.hash = createHash("sha256").update(toHash).digest("hex")
+        manifest.main = "index.js"
+        await writeFile(`./dist/${plug}/manifest.json`, JSON.stringify(manifest))
+
+        console.log(`Successfully built ${manifest.name}!`)
+
     } catch (e) {
-        console.error("Failed to build plugin...", e);
-        process.exit(1);
+        console.error("Failed to build plugin...", e)
+        process.exit(1)
     }
+}
+if (deploy) {
+    const exec = (cmd) => execSync(cmd, { stdio: "inherit" })
+    console.log("Deploying plugin to device...")
+    let isXposed
+
+    try {
+        isXposed = execSync(`adb shell "pm list packages | grep com.vendetta.xposed"`).length > 0
+    } catch {}
+
+    const pkgName = isXposed ? "com.discord" : "dev.beefers.vendetta"
+    exec(`adb shell am force-stop ${pkgName}`)
+    exec(`adb shell am start -n ${pkgName}/com.discord.main.MainActivity`)
 }
