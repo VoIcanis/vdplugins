052c296f8fea5b5a3cc84e8c4d1780f39f0ac7dd
Volcanis <673642+VoIcanis@users.noreply.github.com>
2023-12-04 16:16:41 -0500

    Update DeepL.ts

--- plugins/FRTranslate/src/api/DeepL.ts
+++ plugins/FRTranslate/src/api/DeepL.ts
@@ -5,9 +5,9 @@ const API_URL = "https://api.deeplx.fun/translate"
 const translate = async (text_data: string, source_lang_data: string = "auto", target_lang_data: string, original: boolean = false) => {
     try {
       let dataToSend = {
-        text: text_data,
-        source_lang: source_lang_data,
-        target_lang: target_lang_data
+        "text": `${text_data}`,
+        "source_lang": `${source_lang_data}`,
+        "target_lang": `${target_lang_data}`
       }
       console.log(dataToSend)
         if (original) return { source_lang_data, text_data }
